12b1ae539871c5168cfb815a7d8d87d23f41f8c4
Siful Islam <siful.technext@gmail.com>
2021-03-05 10:32:01 +0600

    space added

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { Container, Form, Row, Col, InputGroup, FormControl, Jumbotron, Button } from 'react-bootstrap';
+import React from "react";
+import {
+    Container,
+    Form,
+    Row,
+    Col,
+    InputGroup,
+    FormControl,
+    Jumbotron,
+    Button,
+} from "react-bootstrap";
 
 const Hero = () => {
     return (
-        <Jumbotron >
+        <Jumbotron>
             <Container>
                 <Row>
                     <Col md={8} className="center">
                         <Form>
                             <InputGroup className="mb-3">
-                                <FormControl
-                                    placeholder="Search Post"
-                                />
+                                <FormControl placeholder="Search Post" />
                                 <InputGroup.Append>
                                     <Button variant="success">Search </Button>
                                 </InputGroup.Append>
